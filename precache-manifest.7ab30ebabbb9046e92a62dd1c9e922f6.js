86f5006fdb6e69482838dc14ca650c7536f47644
Fredrik Kivi <fredrik.kivi@ugli.se>
2021-01-19 23:18:19 +0100

    local-ci

---
+++ precache-manifest.7ab30ebabbb9046e92a62dd1c9e922f6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78eb980732a0294a5a22a1a1207416d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "383e22c4150f2818b39b",
+    "url": "/static/css/main.c5a3ee5c.chunk.css"
+  },
+  {
+    "revision": "f53afc3984ed6affbf2f",
+    "url": "/static/js/2.b2d8994c.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.b2d8994c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "383e22c4150f2818b39b",
+    "url": "/static/js/main.21acb0a5.chunk.js"
+  },
+  {
+    "revision": "deaf17d855cbdaf7344d",
+    "url": "/static/js/runtime-main.47788c5d.js"
+  },
+  {
+    "revision": "fa895b5c452d02e7feecb310886bdbb1",
+    "url": "/static/media/2018.fa895b5c.jpg"
+  },
+  {
+    "revision": "915bfbc4809c9cfb5dc33bc76b977ca8",
+    "url": "/static/media/bg0.915bfbc4.jpg"
+  },
+  {
+    "revision": "bc3c3d4fd6187b77e197338c0d536281",
+    "url": "/static/media/family_blood.bc3c3d4f.jpg"
+  },
+  {
+    "revision": "61dfec3da4fb4ff6664d15673f053783",
+    "url": "/static/media/for_demokrati.61dfec3d.jpg"
+  },
+  {
+    "revision": "5ffd11e9dfbaf0852ce9f55427e7847c",
+    "url": "/static/media/haters_gonna_hate.5ffd11e9.jpg"
+  },
+  {
+    "revision": "31abf03fcd56f29c7cf678f5c03b21d5",
+    "url": "/static/media/historien_om.31abf03f.jpg"
+  },
+  {
+    "revision": "ab4bf6cc56be9c192a32bd42e79cf24a",
+    "url": "/static/media/jag_vill_dansa.ab4bf6cc.jpg"
+  },
+  {
+    "revision": "e3d6bf992db386567a1d57d77a243122",
+    "url": "/static/media/ugli.e3d6bf99.png"
+  },
+  {
+    "revision": "1a8dcc46a515bb85a6c53b6b112398a0",
+    "url": "/static/media/vill_inte_se.1a8dcc46.jpg"
+  }
+]);
